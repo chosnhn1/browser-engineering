917dee72df99a10e0b912105642cb2599bb3b762
Cho Seonghan <chosnhn1@gmail.com>
2025-09-11 17:07:14 +0900

    make text and tag interpreting in tkinter browser

--- browser.py
+++ browser.py
@@ -1,11 +1,12 @@
 import socket
 import ssl
 import tkinter
+import tkinter.font
 
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https"]
+        assert self.scheme in ["http", "https", "file"]
 
         # set up port
         if self.scheme == "http":
@@ -81,34 +82,148 @@ class URL:
 
         return content
 
-
 def lex(body):
-    text = ""
+    out = []
+    buffer = ""
     in_tag = False
     for c in body:
         if c == "<":
             in_tag = True
+            if buffer: out.append(Text(buffer))
+            buffer = ""
         elif c == ">":
             in_tag = False
-        elif not in_tag:
-            text += c
-    return text
+            out.append(Tag(buffer))
+            buffer = ""
+        else:
+            buffer += c
+    
+    if not in_tag and buffer:
+        out.append(Text(buffer))
+    
+    return out
 
 # global variables for Browser
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 
-def layout(text):
+def layout(tokens):
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP
-            cursor_x = HSTEP
+    weight, style = "normal", "roman"
 
+    for tok in tokens:
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                font = tkinter.font.Font(
+                    size=16,
+                    weight=weight,
+                    slant=style,
+                )
+                display_list.append((cursor_x, cursor_y, word, font))
+                w = font.measure(word)
+                # if word overflowed from the line: 
+                if cursor_x + w > WIDTH - HSTEP:
+                    # give linespacing of additional 25%
+                    cursor_y += font.metrics("linespace") * 1.25
+                    cursor_x = HSTEP
+
+                # give each word a padding
+                cursor_x += w + font.measure(" ")
+        elif tok.tag == "i":
+            style = "italic"
+        elif tok.tag == "/i":
+            style = "roman"
+        elif tok.tag == "b":
+            weight = "bold"
+        elif tok.tag == "/b":
+            weight = "normal"
+        
     return display_list
+
+
+class Layout:
+    def __init__(self, tokens):
+        self.display_list = []
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+        self.line = []
+        for tok in tokens:
+            self.token(tok)
+        self.flush()
+
+    def token(self, tok):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+    
+    def word(self, word):
+        # font = tkinter.font.Font(
+        #     size=self.size,
+        #     weight=self.weight,
+        #     slant=self.style,
+        # )
+        font = get_font(self.size, self.weight, self.style)
+        # self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+        w = font.measure(word)
+        # if word overflowed from the line: 
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+
+        self.line.append((self.cursor_x, word, font))   # y will computed later
+        # give each word a padding
+        self.cursor_x += w + font.measure(" ")
+
+    def flush(self):
+        if not self.line : return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+
+        self.cursor_x = HSTEP
+        self.line = []
+
+
+# global font dictionary for caching
+FONTS = {}
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
 
 # global variable for scrolling
 SCROLL_STEP = 100
@@ -126,25 +241,39 @@ class Browser:
         # scroll
         self.scroll = 0
         self.window.bind("<Down>", self.scrolldown)
+        self.display_list = []
 
     def draw(self):
         self.canvas.delete("all")
-        for x, y, c in self.display_list:
-            self.canvas.create_text(x, y - self.scroll, text=c)
+        for x, y, word, font in self.display_list:
+            if y > self.scroll + HEIGHT: continue
+            if y + font.metrics("linespace") < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=word, font=font,
+                                    # anchor: northwest corner, not center is the anchor
+                                    anchor="nw")
     
     # load given URL instance
     def load(self, url):
         body = url.request()
         # parse response body with lex function
-        text = lex(body)
-        self.display_list = layout(text)
+        tokens = lex(body)
+        self.display_list = Layout(tokens).display_list
         self.draw()
 
     # scroll down
     def scrolldown(self, e):
         self.scroll += SCROLL_STEP
         self.draw()
-            
+
+
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
 
 if __name__ == "__main__":
     import sys
